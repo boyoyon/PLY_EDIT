c0ac71de07b886412f8fa1d333d5a5821e8f1b40
boyoyon <nishimaki.hidetoshi@gmail.com>
2026-03-25 12:26:20 +0900

    Add files via upload

---
+++ src/create_back_face_from_front_face.py
@@ -0,0 +1,22 @@
+import numpy as np
+import open3d as o3d
+import os, sys
+
+argv = sys.argv
+argc = len(argv)
+
+print('%s creates back face from the front face' % argv[0])
+print('[usage] python %s <ply>' % argv[0])
+
+if argc < 2:
+    quit()
+
+mesh = o3d.io.read_triangle_mesh(argv[1])
+triangles = np.array(mesh.triangles)
+mesh.triangles = o3d.utility.Vector3iVector(triangles[:,[0,2,1]])
+
+base = os.path.basename(argv[1])
+filename = os.path.splitext(base)[0]
+dst_path = '%s_back_face.ply' % filename
+o3d.io.write_triangle_mesh(dst_path, mesh)
+print('save %s' % dst_path)
