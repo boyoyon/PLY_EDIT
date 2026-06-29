9a75ec0bfd45dba205e26f10ee2f099333d8aeae
boyoyon <nishimaki.hidetoshi@gmail.com>
2026-06-29 15:59:38 +0900

    Add files via upload

--- src/p_polyline.py
+++ src/p_polyline.py
@@ -1,7 +1,21 @@
 import numpy as np
 import open3d as o3d
 import copy
-from polygon import get_rotation_to_vector
+#from polygon import get_rotation_to_vector
+
+def rotate_points_by_axes(points, new_x, new_y):
+    x_axis = new_x / np.linalg.norm(new_x)
+    
+    y_axis = new_y - np.dot(new_y, x_axis) * x_axis
+    y_axis = y_axis / np.linalg.norm(y_axis)
+    
+    z_axis = np.cross(x_axis, y_axis)
+    
+    R = np.column_stack((x_axis, y_axis, z_axis))
+    
+    rotated_points = np.dot(points, R.T)
+    
+    return rotated_points
 
 def p_polyline(points, section):
 
@@ -10,7 +24,7 @@ def p_polyline(points, section):
 
     if nr_points > 1 and len(section) > 0:
 
-        if len(np.array(section)) == 3:
+        if np.array(section).ndim == 3:
             _section0 = np.array(section[-1])
         else:
             _section0 = np.array(section)   
@@ -18,44 +32,36 @@ def p_polyline(points, section):
         # x軸向きになるように回転
         R0 = o3d.geometry.get_rotation_matrix_from_xyz((0, 0, np.pi/2)) 
         _section0 = _section0 @ R0.T   
- 
-        _sectionA = _section0
-        _sectionB = _section0
 
         # パイプの作成 
+
+        prevYaxis = None
+
         for i in range(1, nr_points-1):
 
             A = np.array(points[i-1])
             B = np.array(points[i])
             C = np.array(points[i+1])        
 
-            y_axis = np.array([0.0, 1.0, 0.0])
-            Y_AXIS = np.cross(C-B, B-A)
-     
-            R3 = get_rotation_to_vector(Y_AXIS, y_axis)
-                
-            if np.allclose(R3,-np.eye(3), atol=1e-8):
-                #print('R3 nearly equals -np.eye(3)')
-                R3 = o3d.geometry.get_rotation_matrix_from_xyz((np.pi, 0, 0)) 
-                #R3 = -np.eye(3)
-
-            x_axis = R3 @ np.array([1.0, 0.0, 0.0])
-            X_AXIS = B - A
+            y_axis = np.cross(C-B,B-A)
+            y_scale = np.clip(np.linalg.norm(y_axis),0,1) ** 2
  
-            R4 = get_rotation_to_vector(X_AXIS, x_axis)
-            if np.allclose(R4,-np.eye(3), atol=1e-8):
-                #print('R4 nearly equal -np.eye(3)')
-                R4 = -np.eye(3)
+            if prevYaxis is not None:            
+                y_axis = (1 - y_scale) * prevYaxis + y_scale * y_axis
 
-            R = R4
+            X_AXIS = B - A
 
+            rotated = rotate_points_by_axes(_section0, X_AXIS, y_axis)
+ 
             if i == 1:
-                p2.append(_section0 @ R.T + A)
+                p2.append(rotated + A)
 
-            p2.append(_section0 @ R.T + B)
+            p2.append(rotated + B)
 
             if i == nr_points - 2:
-                p2.append(_section0 @ R.T + C)
+                p2.append(rotated + C)
+
+            prevYaxis = y_axis
 
     return p2
 
